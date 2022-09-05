b5c01f51a0188af5b9bdf8677509f817e8ddd7c6
ostrichick <95114705+ostrichick@users.noreply.github.com>
2022-09-05 15:13:02 +0900

    todo list update

--- MaruProject/src/main/java/ezen/maru/pjt/controller/ProuctController.java
+++ MaruProject/src/main/java/ezen/maru/pjt/controller/ProuctController.java
@@ -1,10 +1,26 @@
 package ezen.maru.pjt.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ezen.maru.pjt.service.product.ProductService;
 
 @Controller
 @RequestMapping("/product")
 public class ProuctController {
+	ProductService addService, listService, editService, deleteService;
 
+	@Autowired(required = false)
+	public void setInsertService(@Qualifier("p_add") ProductService addService) {
+		this.addService = addService;
+	}
+
+	// 관리자 전용
+	@GetMapping("/add")
+	public String add() {
+		return "product/add";
+	}
 }
